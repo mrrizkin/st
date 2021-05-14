13eabfe7bb7f5551b2cd5a4161898d5cbd64ed82
nugrhrizki <nugraha07rizki@gmail.com>
2021-05-14 05:11:23 +0000

    use iosevka

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Iosevka:pixelsize=14:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
 	"Hack Nerd Font:pixelsize=12:antialias=true:autohint=true",
