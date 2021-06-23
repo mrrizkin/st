1404cc76db9c1467c666ce75c276f84a3bd1cd8e
nugrhrizki <nugraha07rizki@gmail.com>
2021-06-23 10:24:45 +0700

    using bigger fonts

--- config.def.h
+++ config.def.h
@@ -5,11 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono Nerd Font:pixelsize=12:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-  "Hack Nerd Font:pixelsize=12:antialias=true:autohint=true",
-  "JoyPixels:pixelsize=12:antialias=true:autohint=true",
 };
 
 static int borderpx = 0;
